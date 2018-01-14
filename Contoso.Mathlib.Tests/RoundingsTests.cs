dbd45067598a7eae8065ace33f35fed67107c50d
Toma Bussarov <toma.bussarov@hotmail.com>
2018-01-14 17:40:39 +0200

    Add project files.

---
+++ Contoso.Mathlib.Tests/RoundingsTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contoso.Mathlib.Tests
+{
+    [TestClass]
+    public class RoundingsTests
+    {
+        [TestMethod]
+        [Description("Rounds down for positive numbers with low decimal part.")]
+        public void Floor_PositiveLow()
+        {
+            // Arrange
+            double d = 123.123;
+
+            // Act
+            var res = Contoso.Mathlib.Roundings.Floor(d);
+
+            // Assert
+            Assert.AreEqual(123, res);
+        }
+        [TestMethod]
+        [Description("Rounds down for positive numbers with high decimal part.")]
+        public void Floor_PositiveHigh()
+        {
+            // Arrange
+            double d = 123.999;
+
+            // Act
+            var res = Contoso.Mathlib.Roundings.Floor(d);
+
+            // Assert
+            Assert.AreEqual(123, res);
+        }
+
+        [TestMethod]
+        [Description("Rounds down for negative numbers with high decimal part.")]
+        public void Floor_NegativeHigh()
+        {
+            // Arrange
+            double d = -123.999;
+
+            // Act
+            var res = Contoso.Mathlib.Roundings.Floor(d);
+
+            // Assert
+            Assert.AreEqual(-123, res);
+        }
+
+        [TestMethod]
+        [Description("Rounds up for positive numbers with high decimal part.")]
+        public void Ceiling_PositiveHigh()
+        {
+            // Arrange
+            double d = 123.74;
+
+            // Act
+            var res = Contoso.Mathlib.Roundings.Ceiling(d);
+
+            // Assert
+            Assert.AreEqual(124, res);
+        }
+
+        [TestMethod]
+        [Description("Rounds up for positive numbers with low decimal part.")]
+        public void Ceiling_PositiveLow()
+        {
+            // Arrange
+            double d = 123.499;
+
+            // Act
+            var res = Contoso.Mathlib.Roundings.Ceiling(d);
+
+            // Assert
+            Assert.AreEqual(124, res);
+        }
+    }
+}
---
+++ Contoso.Mathlib/Roundings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Contoso.Mathlib
+{
+    public static class Roundings
+    {
+        public static int Floor(double d)
+        {
+            return (int)d;
+        }
+
+        public static int Ceiling(double d)
+        {
+            return (int)(d + 1);
+        }
+
+        public static void DummyMethod()
+        {
+            bool b = true;
+            if (b)
+            {
+                Console.WriteLine("djksfhjskdfh");
+            }
+        }
+
+    }
+
+}
